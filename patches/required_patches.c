851f508d09fdf374e5fc0e4eda3ac96d75ecb6e4
RevoSucks <projectrevotpp@hotmail.com>
2026-01-23 00:11:04 -0500

    more work. manual patch in func_80297C7C is required. Recomp doesnt support this particular auto patch

--- patches/required_patches.c
+++ patches/required_patches.c
@@ -86,3 +86,157 @@ RECOMP_PATCH void set_zero_vaddr_tlb(void) {
     //osMapTLB(0, 0, NULL, (u32) (((u32) (&D_80042000)) - 0x80000000), -1, -1);
     D_8001BFC0[0] = 0x80019f80; // this feels dirty hardcoding it, but whatever.
 }
+
+extern s32 D_802A5328;
+extern s32 D_802A5324;
+
+extern u8 gOverlaySizes[][2];
+extern u8 D_8019C0D0[][3];
+
+typedef int (*OverlayCallback)(void);
+
+void func_80226368(s32);
+void func_80297D58(s32, s32);
+void func_80297D50(s32, s32);
+void func_80292BD0(s32 arg0, u8* arg1, s32 arg2);
+void func_80297D60(s32, s32);
+void func_802266E8(s32);
+s32 func_80226604(s32, s32);
+s32 func_802266DC(s32);
+void func_8022616C(void*, s32, s32, s32);
+
+
+u32 gGlobalOverlayROM_Base   = 0x00000000; // the currently set ROM offset of the OVL being loaded.
+u32 gGlobalOverlayROM_Offset = 0x00000000; // the offset of the overlay
+
+extern u32 D_802AC5DC;
+
+// -------------------------
+extern int func_ovl3_1_80043000(void); // ovl_3_subovl_1 (Copyright Screen)
+extern int func_ovl2_5_80043000(void); // ovl_2_subovl_5 (Intro) ?
+
+RECOMP_PATCH void func_8022691C(s32 arg0) {
+    s32 sp44;
+    s32 sp40;
+    s32 sp3C;
+    s32 temp_a0;
+    s32 temp_v0;
+    s32 target;
+    s32 pad[4];
+
+    if (arg0 == D_802A5328) {
+        return;
+    }
+
+    D_802A5328 = arg0;
+    temp_a0 = (gOverlaySizes[arg0][0] << 17) + (gOverlaySizes[arg0][1] << 11);
+    if (temp_a0 != D_802A5324) {
+        D_802A5324 = temp_a0;
+        func_802266E8(temp_a0);
+    }
+    sp44 = func_80226604(0, 1);
+    sp40 = func_802266DC(sp44);
+    func_8022616C((s32* )D_8019C0D0, 1, sp40, sp44);
+    target = D_8019C0D0[0][0];
+    for (sp40 = 0; sp40 < target; sp40++) {
+        sp3C = (D_8019C0D0[sp40][1] << 8) + D_8019C0D0[sp40][2];
+        if (sp3C == arg0) {
+            sp3C = D_8019C0D0[sp40 + 1][0];
+            break;
+        }
+    }
+    func_80226368(sp44);
+    sp44 = func_80226604(sp3C, 1);
+    func_8022616C(&sp40, 4, 1, sp44);
+    func_80297D58(0x80043000, 0x20000);
+    func_80297D50(0x80043000, 0x20000);
+    func_80292BD0(sp44, (u8*)0x80043000, sp40);
+    func_80297D60(0x80043000, 0x20000);
+    func_80226368(sp44);
+    if (D_802A5324 != 0x300000) {
+        D_802A5324 = 0x300000;
+        func_802266E8(0x300000);
+    }
+    // We dont need to store the variable, as we are interpreting the file ID later when the overlay gets executed.
+    //D_802A532C = ((OverlayCallback)0x80043000)();
+
+    /*
+    switch(D_802AC5DC) {
+        case 0x00: func_ovl3_1_80043000(); return;
+        case 0x1B: func_ovl2_5_80043000(); return;
+        default: // unsupported overlay.
+            while(1)
+               ;
+    }
+    */
+}
+
+extern u32 D_802A1D90;
+extern u8 *D_802B0310;
+extern u16 D_802B0314;
+
+extern void func_8029ADCC(void*);
+extern s32 func_80226304(s32);
+u8* Libc_Memset(u8* arg0, u8* arg1, s32 arg2);
+
+extern u8 *func_802998EC(int);
+
+RECOMP_PATCH void func_80292BD0(s32 arg0, u8* arg1, s32 arg2) {
+    s32 size;
+    s32 i;
+    u16 var_s5;
+    s32 temp_s4;
+    u8* sp44;
+    s32 temp_v0_3;
+
+    sp44 = arg1;
+    D_802A1D90 = 1;
+    D_802B0310 = func_802998EC(0x400);
+    D_802B0314 = 0x3BE;
+    Libc_Memset(D_802B0310, 0, 0x400);
+    var_s5 = 0;
+    while (arg2 != 0) {
+        var_s5 >>= 1;
+        if (!(var_s5 & 0x100)) {
+            var_s5 = func_80226304(arg0) | 0xFF00;
+        }
+        if (var_s5 & 1) {
+            D_802B0310[D_802B0314] = (*arg1++) = func_80226304(arg0);
+            arg2 -= 1;
+            D_802B0314++;
+            D_802B0314 &= 0x3FF;
+        } else {
+            temp_s4 = func_80226304(arg0);
+            temp_v0_3 = func_80226304(arg0);
+            temp_s4 |= ((temp_v0_3 & ~0x3F) * 4);
+            gGlobalOverlayROM_Offset = temp_s4;
+            size = (temp_v0_3 & 0x3F) + 3;
+            for (i = 0; i < size; i++) {
+                D_802B0310[D_802B0314] = (*arg1++) = D_802B0310[(temp_s4 + i) & 0x3FF];
+                D_802B0314++;
+                D_802B0314 &= 0x3FF;
+            }
+            arg2 -= i;
+        }
+    }
+    func_80297D60(((u32) (sp44 + 0xF) >> 4) * 0x10, ((u32) (arg2 + 0xF) >> 4) * 0x10);
+    func_8029ADCC(D_802B0310);
+    D_802A1D90 = 0;
+}
+
+extern int (*D_802A532C)();
+
+extern void func_ovl3_1_80044108();
+extern void func_ovl2_5_80047AF4();
+
+// execute overlay
+RECOMP_PATCH void func_802268F4(void) {
+    switch(D_802AC5DC) {
+        case 0x00: func_ovl3_1_80044108(); return; // Copyright
+        case 0x1B: func_ovl2_5_80047AF4(); return; // Intro
+        default: // unsupported overlay.
+            while(1)
+               ;
+    }
+    //D_802A532C();
+}
